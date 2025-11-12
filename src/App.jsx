62d43843634ab154fb2b213cf89889cd8674f552
Blinchik <ak130504666@gmail.com>
2025-11-13 02:54:45 +0300

    mobX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+/*import { useState } from "react";*/
 import "./App.module.css";
 import Button from "./components/Button/Button";
 import Number from "./components/Content/Number";
-function App() {
-  const [count, setCount] = useState(0);
+import ObservableCountStore from "./store/counter";
+import { observer } from "mobx-react-lite";
+
+const App = observer(() => {
+  /*const [count, setCount] = useState(0);
 
   const increaseNumber = () => {
     setCount(count + 1);
@@ -13,27 +16,39 @@ function App() {
     setCount(count - 1);
   };
 
-  /*//function decreaseNumber() {
-//    setCount(count - 1);
-//}
+  function decreaseNumber() {
+    setCount(count - 1);
+}
 
-//const decreaseNumber = function() {
-//    setCount(count - 1);
-//};*/
+const decreaseNumber = function() {
+    setCount(count - 1);
+};*/
 
   return (
     <div className="app">
-      <Number value={count} />
+      <Number value={ObservableCountStore.count} />
       <div className="buttons">
-        <Button onClick={increaseNumber} variant="primary">
+        {/*<Button onClick={increaseNumber} variant="primary">
           <span>+1</span>
         </Button>
         <Button onClick={decreaseNumber} variant="notprimary">
+          <span>-1</span>
+        </Button> */}
+        <Button
+          onClick={() => ObservableCountStore.increase()}
+          variant="primary"
+        >
+          <span>+1</span>
+        </Button>
+        <Button
+          onClick={() => ObservableCountStore.decrease()}
+          variant="notprimary"
+        >
           <span>-1</span>
         </Button>
       </div>
     </div>
   );
-}
+});
 
 export default App;
